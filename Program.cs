b474a144b17304073514eb633381715db7946111
niles <niles@LAPTOP-3K24CQ1R>
2021-09-08 22:54:48 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleAppExtensionMethodEgByClass
+{
+    public class OldClass
+    {
+        public int x = 100;
+        public void Test1()
+        {
+            Console.WriteLine("Method one : " + this.x);
+        }
+        public void Test2()
+        {
+            Console.WriteLine("Method two : " + this.x);
+        }
+    }
+    public static class NewClass
+    {
+       
+        public static void Test3(this OldClass O)
+        {
+            Console.WriteLine("Method Three" );
+        }
+        public static void Test4(this OldClass O,int x)
+        {
+            Console.WriteLine("Method Four : " + x);
+        }
+
+        public static void Test5(this OldClass O)
+        {
+            Console.WriteLine("Method Five : " + O.x);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OldClass obj = new OldClass(); 
+            obj.Test1();
+            obj.Test2();
+            //Calling extension methods
+            obj.Test3();
+            obj.Test4(10);
+            obj.Test5();
+            Console.ReadLine();
+            
+        }
+    }
+}
